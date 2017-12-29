b5b8d559639e5246e3ace3b81d472707d2335a8a
Doug Winter <doug.winter@isotoma.com>
2017-12-29 09:56:17 +0000

    attempt at yaml

--- config.go
+++ config.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	"gopkg.in/yaml.v2"
 )
+
+type Link struct {
+	Profile string
+	Context string
+}
 
 type ConfigCommand struct {
 }
 
 var configCommand ConfigCommand
 
+func setContext(profile string, context string) {
+	fmt.Printf("Setting profile to %s for context %s\n", profile, context)
+	var l []Link
+	doc, err := ioutil.ReadFile("k8ecr.yaml")
+	if err == nil {
+		yaml.Unmarshal(doc, &l)
+	}
+	k := append(l, Link{profile, context})
+	d, err := yaml.Marshal(&k)
+	if err == nil {
+		ioutil.WriteFile("k8ecr.yaml", d, 0644)
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
 func (x *ConfigCommand) Execute(args []string) error {
-	fmt.Printf("Setting profile to %s\n", args[0])
+	cmd := exec.Command("kubectl", "config", "current-context")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	context := strings.TrimSpace(string(output))
+	setContext(args[0], context)
 	return nil
 }
 
