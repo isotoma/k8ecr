2352d0551c21f61b9914802a9ac0a927635e056f
Doug Winter <doug.winter@isotoma.com>
2017-12-30 15:05:38 +0000

    pushing actually runs

--- push.go
+++ push.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"strings"
 
 	"encoding/base64"
+	"encoding/json"
 
 	docker "docker.io/go-docker"
 	"docker.io/go-docker/api/types"
@@ -38,35 +41,76 @@ func getCredentials() (types.AuthConfig, error) {
 	}, nil
 }
 
-func login() (*docker.Client, string, error) {
+func login() (*docker.Client, types.AuthConfig, error) {
 	creds, err := getCredentials()
 	if err != nil {
-		return nil, "", err
+		return nil, creds, err
 	}
 	cli, err := docker.NewEnvClient()
 	if err != nil {
-		return cli, "", err
+		return cli, creds, err
 	}
 	_, err = cli.RegistryLogin(context.Background(), creds)
 	if err != nil {
-		return nil, "", err
+		return nil, creds, err
 	}
-	return cli, creds.ServerAddress[8:], nil
+	return cli, creds, nil
 }
 
-func push(client *docker.Client, endpoint string, repo string, version string) error {
+func registryAuth(creds types.AuthConfig) string {
+	b := fmt.Sprintf("%s:%s", creds.Username, creds.Password)
+	return base64.StdEncoding.EncodeToString([]byte(b))
+}
+
+func tag(client *docker.Client, endpoint string, repo string, version string) error {
 	source := fmt.Sprintf("%s:%s", repo, version)
 	target := fmt.Sprintf("%s:%s", endpoint, version)
 	return client.ImageTag(context.Background(), source, target)
 }
 
+func push(client *docker.Client, creds types.AuthConfig, repo string, version string) error {
+	err := tag(client, creds.ServerAddress[8:], repo, version)
+	if err != nil {
+		return err
+	}
+	image := fmt.Sprintf("%s/%s", creds.ServerAddress[8:], repo)
+	stream, err := client.ImagePush(context.Background(),
+		image,
+		types.ImagePushOptions{
+			RegistryAuth: registryAuth(creds),
+		})
+	if err != nil {
+		return err
+	}
+	buffered := bufio.NewReader(stream)
+	for {
+		b, err := buffered.ReadBytes('\n')
+		if err != nil {
+			stream.Close()
+			return err
+		}
+		data := make(map[string]interface{})
+		jsonErr := json.Unmarshal(b, &data)
+		if jsonErr != nil {
+			stream.Close()
+			return jsonErr
+		}
+		fmt.Println(data)
+		if err == io.EOF {
+			fmt.Println("\nDone")
+			stream.Close()
+			return nil
+		}
+	}
+}
+
 func pushRepository(name string, versions []string) error {
-	cli, endpoint, err := login()
+	cli, creds, err := login()
 	if err != nil {
 		return err
 	}
 	for _, v := range versions {
-		err := push(cli, endpoint, name, v)
+		err := push(cli, creds, name, v)
 		if err != nil {
 			return err
 		}
@@ -74,9 +118,10 @@ func pushRepository(name string, versions []string) error {
 	return nil
 }
 
+// Execute the push command
 func (x *PushCommand) Execute(args []string) error {
 	if len(args) < 2 {
-		return errors.New("push REPOSITORY VERSION...")
+		return errors.New("push REPOSITORY VERSION")
 	}
 	return pushRepository(args[0], args[1:])
 
