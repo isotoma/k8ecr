614f9bd2895cfdc47e19f555398a8d1c5bc3135a
Andrew Plummer <plummer574@gmail.com>
2018-06-30 16:40:02 +0100

    No webhook by default

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ RUN apk add --no-cache ca-certificates
 ADD k8ecr /
 ADD autodeploy.py /
 RUN pip install requests 
-ENV WEBHOOK https://hooks.slack.com/services/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
 CMD ./autodeploy.py
--- autodeploy.py
+++ autodeploy.py
@@ -7,11 +7,11 @@ import json
 import time
 
 namespace = os.environ['NAMESPACE']
-webhook = os.environ.get('WEBHOOK', None)
+webhook = os.environ.get('WEBHOOK')
 
 while True:
     p = subprocess.run(["./k8ecr", "deploy", namespace, "-"], stdout=subprocess.PIPE)
-    if webhook is not None and p.stdout is not None:
+    if webhook and p.stdout is not None:
         requests.post(
             webhook,
             data=json.dumps({'text': p.stdout.decode("utf-8")}),
